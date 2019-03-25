d2f738041ea67b8cad63088d39e14040343cca06
Moritz <44192283+Moritzheine@users.noreply.github.com>
2019-03-25 16:42:47 +0100

    Update HM3301.ts

--- HM3301.ts
+++ HM3301.ts
@@ -6,31 +6,61 @@
 
 enum DustValues {
     //% block="P1.0"
-    P1 = 0,
+    P1 = 2,
     //% block="P2.5"
-    P2 = 1,
+    P2 = 3,
     //% block="P10.0"
-    P10 = 2
+    P10 = 4
 }
 
 namespace HM3301 {
     let ADDRESS = 0x40;
+
+    function checkReceived(buf: Buffer): boolean {
+        if (buf.length != 29) {
+            return false
+        }
+        let sum = -1
+        for (let j = 2; j < 28; j++) {
+            sum += buf[j]
+        }
+    
+        if (sum != buf[28]) {
+            return false
+        }
+        return true
+    }
+
+    function readBytes(num: number) {
+        let buf = pins.i2cReadBuffer(ADDRESS, num, true)
+        if (buf.length != num) {
+            return []
+        }
+        return buf
+    }
+
     /**
      * Read Dust Value
      */
     //% blockId=HM3301_read_value
-    //% block="Read dust value %i "
-    export function readDustValue(i: DustValues):void {
-        
+    //% block="Read dust value|%i "
+    export function readDustValue(i: DustValues): number {
+        // read 29 bytes from the sensor
+        let bytes = readBytes(29)
+        if (!checkReceived(bytes)) {
+            // Error Value
+            return -1
+        }
+        let value = bytes[i * 2] << 8 | bytes[i * 2 + 1]
+        return value
     }
+    
     /**
      * Init
      */
     //% blockId=HM3301_init
     //% block="Initialize the sensor"
-    export function init():void {
-        
+    export function init(): void {
+        pins.i2cWriteNumber(ADDRESS, 0x88, NumberFormat.Int16BE, false)
     }
-
-
 }
